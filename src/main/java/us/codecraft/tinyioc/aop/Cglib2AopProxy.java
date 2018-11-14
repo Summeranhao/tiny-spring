b3b9205eb14ee9ac455951cc8724622d5644b2eb
wanghui <wanghui@ec.e-chemchina.com>
2018-11-14 14:00:55 +0800

    12

--- src/main/java/us/codecraft/tinyioc/aop/Cglib2AopProxy.java
+++ src/main/java/us/codecraft/tinyioc/aop/Cglib2AopProxy.java
@@ -7,7 +7,7 @@ import net.sf.cglib.proxy.MethodProxy;
 import java.lang.reflect.Method;
 
 /**
- * @author yihua.huang@dianping.com
+ * @author
  */
 public class Cglib2AopProxy extends AbstractAopProxy {
 
